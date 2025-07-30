7d3cc0ff03ad7c45544e6abeb3b00a9751860fe6
Nasim <mdnasimhosen333@gmail.com>
2025-07-30 20:48:34 +0600

    Add implementation for All Odd Prefix Sums problem in CodeChef contest

---
+++ first_contest/All_Odd_Prefix_Sums.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int T;
+    cin >> T;
+    while (T--)
+    {
+        int N;
+        cin >> N;
+        int odd = 0, even = 0;
+        for (int i = 0; i < N; ++i)
+        {
+            int x;
+            cin >> x;
+            if (x % 2 == 0)
+                even++;
+            else
+                odd++;
+        }
+        if (odd == 1)
+            cout << "Yes" << endl;
+        else
+            cout << "No" << endl;
+    }
+    return 0;
+}
