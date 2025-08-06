2cc04f5e4f19457606a732533f7474139ecd0e0f
Nasim <mdnasimhosen333@gmail.com>
2025-08-06 20:53:33 +0600

    Refactor Currency Exchange solution to use BFS for improved efficiency

--- second_contest/Currency_Exchange.cpp
+++ second_contest/Currency_Exchange.cpp
@@ -1,25 +1,7 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-bool possible(int a1, int b1, int a2, int b2)
-{
-    int max_d = min(a1, b1);
-    for (int d = 0; d <= max_d; d++)
-    {
-        int g = a1 - d, s = b1 - d;
-        int dg = a2 - g, ds = b2 - s;
-        if (g - a2 == (b2 - s) / 5 && (b2 - s) % 5 == 0)
-        {
-            int x = g - a2;
-            int gold = g, silver = s;
-            gold -= x;
-            silver += 5 * x;
-            if (gold >= 0 && silver >= 0 && gold == a2 && silver == b2)
-                return true;
-        }
-    }
-    return false;
-}
+bool visited[1001][1001]; // faster than set
 
 int main()
 {
@@ -29,7 +11,46 @@ int main()
     {
         int a1, b1, a2, b2;
         cin >> a1 >> b1 >> a2 >> b2;
-        cout << (possible(a1, b1, a2, b2) ? "Yes" : "No") << endl;
+
+        memset(visited, false, sizeof(visited));
+
+        queue<pair<int, int>> q;
+        q.push({a1, b1});
+        visited[a1][b1] = true;
+
+        bool found = false;
+
+        while (!q.empty())
+        {
+            auto [g, s] = q.front();
+            q.pop();
+
+            if (g == a2 && s == b2)
+            {
+                found = true;
+                break;
+            }
+
+            if (g > 0 && s > 0 && !visited[g - 1][s - 1])
+            {
+                visited[g - 1][s - 1] = true;
+                q.push({g - 1, s - 1});
+            }
+
+            if (g > 0 && s + 5 <= 1000 && !visited[g - 1][s + 5])
+            {
+                visited[g - 1][s + 5] = true;
+                q.push({g - 1, s + 5});
+            }
+
+            if (s >= 5 && g + 1 <= 1000 && !visited[g + 1][s - 5])
+            {
+                visited[g + 1][s - 5] = true;
+                q.push({g + 1, s - 5});
+            }
+        }
+
+        cout << (found ? "Yes" : "No") << endl;
     }
     return 0;
 }
